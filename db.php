c94675e18feacc5ad7dae22de2c775f4a4d11898
Daneo <ruizdaneo@gmail.com>
2025-11-10 22:19:49 -0700

    Soporte para variables de entorno en Railway

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
-$host = 'switchyard.proxy.rlwy.net';
-$port = 12014;
-$user = 'root';
-$password = 'TaqXGlSrbEExYMYKCrhcvSxSIrMuMbFT';
-$database = 'railway';
+$host = getenv('MYSQLHOST') ?: 'switchyard.proxy.rlwy.net';
+$port = getenv('MYSQLPORT') ?: 12014;
+$user = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: 'TaqXGlSrbEExYMYKCrhcvSxSIrMuMbFT';
+$database = getenv('MYSQLDATABASE') ?: 'railway';
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("❌ Error de conexión: " . $conn->connect_error);
+} else {
+    echo "✅ Conexión exitosa a MySQL";
 }
 ?>
-
